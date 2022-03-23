ede0fe92ce3409a1b97064f3ef828cc246b3cb9d
loriful <tiptonica@gmail.com>
2022-03-23 00:31:50 -0400

    MVP

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ⬢ serene-wildwood-86248
 https://serene-wildwood-86248.herokuapp.com/ | https://git.heroku.com/serene-wildwood-86248.git
+
+Your challenge is to create an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+The application’s front end has already been created. It's your job to build the back end, connect the two, and then deploy the entire application to Heroku.
--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
+const { findById, createNewNote } = require('../../lib/notes');
+const { notes } = require('../../data/db');
+  
+  
+  router.get('/notes', (req, res) => {
+      res.json(notes);
+  });
 
+  router.post('/notes', (req, res) => {
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+  });
 
+  router.get('/notes/:id', (req, res) => {
+      const result = findById(req.params.id, notes);
+      if (result) {
+        res.json(result);
+      } else {
+        res.send(404).send('Item not found.');
+      }
+  });
+  
 module.exports = router;
